826a4ab82e451344df03f0a72c1430ac77b14dbb
Lucas-Mc <lumccul3@gmail.com>
2019-07-30 02:48:03 -0600

    Added stack and pointer practice

--- find_volume.cpp
+++ find_volume.cpp
@@ -84,6 +84,7 @@ int main()
     cout << row_val << " " << col_val << endl;
     twoDmat[row_val][col_val] = i;
   }
+  // Print out the 2-D static array
   for (int i = 0; i < 2; i++)
   {
     for (int j = 0; j < 2; j++)
@@ -91,6 +92,27 @@ int main()
       cout << "twoDmat[" << to_string(i) << "]["<< to_string(j) << "] = " << twoDmat[i][j] << endl;
     }
   }
+  // Compare some address fundamentals
+  int a = 0;
+  int* b = &a;                                    // b is a pointer to the address of variable a
+  cout << "b = " << b << "\ta = 0" << endl;
+  cout << "*b = " << *b << "\t\ta = 0" <<  endl;  // *b dereferences b from the address of a to its value
+  a++;
+  cout << "b = " << b << "\ta = 1" <<  endl;
+  cout << "*b = " << *b << "\t\ta = 1" <<  endl;  
+  *b = 2;
+  cout << "b = " << b << "\ta = *b = 2" <<  endl;
+  cout << "*b = " << *b << "\t\ta = *b = 2" <<  endl;    
+  // Investigate the stack locations
+  short int ad1 = 0;
+  int ad2 = 0;
+  long long int ad3 = 0;
+  int ad4 = 0;
+  cout << "&ad1 = " << &ad1 << "\tshort int"<< endl;
+  cout << "&ad2 = " << &ad2 << "\tint" << endl; 
+  cout << "&ad3 = " << &ad3 << "\tlong long int" << endl;
+  cout << "&ad4 = " << &ad4 << "\tint" << endl;     
+  // Print out the computed function values
   string output_string = "Hello World!";
   cout << output_string << endl;
   cout << "Number of parameters needed: " << determine_shape_params("cube") << endl;
